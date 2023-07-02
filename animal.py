d609e51be5d273e4afe676ae071d3d12aacd380a
Ellie Peterson <elliepeterson@Ellies-Air-2.home>
2023-07-01 22:55:31 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,9 @@
+# Author: Ellie Peterson
+# GitHub username:
+# Date: 06/30/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favoirte animal
+
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
+
---
+++ main.py
@@ -0,0 +1,8 @@
+# Author: Ellie Peterson
+# GitHub username:
+# Date: 06/30/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
